0c1b4bcf18923e45d231bb4e3ebf9ff4212d1986
dianacamacho <dcam1901@yahoo.com>
2017-03-26 00:01:55 -0500

    in progress, moving weather to service

--- app/assets/javascripts/angular/controllers/forecast_ctrl.js
+++ app/assets/javascripts/angular/controllers/forecast_ctrl.js
@@ -1,18 +1,10 @@
 (function() {
-  angular.module("app").controller("forecastController", ["$scope", "$log", "$resource", "$routeParams", "cityService", function($scope, $log, $resource, $routeParams, cityService) {
+  angular.module("app").controller("forecastController", ["$scope", "$log", "$resource", "$routeParams", "cityService", "weatherService", function($scope, $log, $resource, $routeParams, cityService, weatherService) {
     $log.log("forecast page");
     $scope.city = cityService.city;
     $scope.days = $routeParams.days || 2;
 
-    var weatherApiPath = "http://api.openweathermap.org/data/2.5/forecast/daily";
-    var weatherRequest = $resource(weatherApiPath);
-    var cityParams = { q: $scope.city, cnt: $scope.days, appid: "da24829670b77e8c573a24a1543be0e7" };
-
-    weatherRequest.get(cityParams, function(result) {
-      $scope.weatherResult = result.list;
-    }, function(errors) {
-      $scope.errors = errors;
-    });
+    $scope.weatherResult = weatherService.getWeather($scope.city, $scope.days);
 
     $scope.convertToFahrenheit = function(degK) {
       var degF = Math.round(1.8 * (degK - 273.15) + 32);
--- app/assets/javascripts/angular/services.js
+++ app/assets/javascripts/angular/services.js
@@ -2,3 +2,20 @@
 angular.module("app").service('cityService', function() {
   this.city = 'Chicago, IL';
 });
+
+angular.module("app").service('weatherService', ['$resource', function($resource) {
+  var weatherApiPath = "http://api.openweathermap.org/data/2.5/forecast/daily";
+  
+  this.getWeather = function(city, days) {
+    var weatherRequest = $resource(weatherApiPath);
+    var cityParams = { q: city, cnt: days, appid: "da24829670b77e8c573a24a1543be0e7" };
+    return {
+      weatherRequest.get(cityParams, function(result) {
+        return result.list;
+        console.log(result);
+      }, function(errors) {
+        return errors;
+      });
+    }
+  }
+}])
