821fc7ea35d27f647ccd0e339587075b7ca37fe0
dianacamacho <dcam1901@yahoo.com>
2017-03-25 23:27:53 -0500

    change home page input to be form using ng-submit allowing for user to use enter key to submit

--- app/assets/javascripts/angular/controllers/home_ctrl.js
+++ app/assets/javascripts/angular/controllers/home_ctrl.js
@@ -1,10 +1,14 @@
 (function() {
-  angular.module("app").controller("homeController", ['$scope', '$log', 'cityService', function($scope, $log, cityService) {
+  angular.module("app").controller("homeController", ['$scope', '$log', '$location', 'cityService', function($scope, $log, $location, cityService) {
     $log.log('home page');
     $scope.city = cityService.city;
 
     $scope.$watch('city', function() {
       cityService.city = $scope.city;
     });
+
+    $scope.submit = function() {
+      $location.path("/forecast");
+    };
   }]);
 })()
