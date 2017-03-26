2441d29d4ca2f23d4a4344b0a69dcc62a7f2e021
dianacamacho <dcam1901@yahoo.com>
2017-03-26 15:59:39 -0500

    weather to service

--- app/assets/javascripts/angular/services.js
+++ app/assets/javascripts/angular/services.js
@@ -3,24 +3,11 @@ angular.module("app").service('cityService', function() {
   this.city = 'Chicago, IL';
 });
 
-angular.module("app").factory('weatherService', ['$resource', function($resource) {
+angular.module("app").service('weatherService', ['$resource', function($resource) {
   var weatherApiPath = "http://api.openweathermap.org/data/2.5/forecast/daily";
   var weatherRequest = $resource(weatherApiPath);
   
-  
-  
-    return {
-      getWeather: function(cityParams) {
-        return weatherRequest.get(cityParams, function(result) {
-          console.log("SUCCESS RESULT");
-          console.log(result);
-          return result;
-        }, function(errors) {
-          console.log("ERROR");
-          console.log(errors);
-          return errors;
-        });
-      }
-    }
-  
+  this.getWeather = function(cityParams) {
+    return weatherData = weatherRequest.get(cityParams);
+  }
 }]);
